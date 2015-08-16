21cdc04c2e6de490a8a7519cddac6df1b884306b
bambusa <beny-ulms@gmx.de>
2015-08-16 22:28:16 +0200

    FIX: IM1 Table

--- PHP Database/Fahrten.php
+++ PHP Database/Fahrten.php
@@ -47,20 +47,16 @@ $query = "SELECT fzgf.ABFAHRT, fzgf.LINROUTENAME, fzgf.FZPROFILNAME FROM fahrzeu
             "LEFT JOIN fzgfahrt fzgf ON fzgfa.FZGFAHRTNR = fzgf.NR ".
             "WHERE fzgfa.VTAGNR = 2 ".
             "ORDER BY fzgfa.FZGFAHRTNR ASC";
+$batch = array();
 
 if ($fzgfahrten = $mysqli->query($query)) {
     printf("Fahrzeugfahrten: %s<br>", $fzgfahrten->num_rows);
 
     $first = true;
     $count = 0;
-    $j = 0;
     $jquery = "INSERT INTO im1 VALUES ";
     $jcount = 0;
     while ($row = $fzgfahrten->fetch_assoc()) {
-        $j++;
-//        if ($first) {
-//            printf("Erste Fahrt: Abfahrt %s, Linie %s, FZProfil %s <br>", $row["ABFAHRT"], $row["LINROUTENAME"], $row["FZPROFILNAME"]);
-//        }
 
         $query = "SELECT fzpe.ANKUNFT, fzpe.ABFAHRT, l.PUBDIVALINNAM, hs.NR, hs.NAME, hs.XKOORD, hs.YKOORD FROM fahrzeitprofilelement fzpe ".
                     "LEFT JOIN linie l ON fzpe.LINNAME = l.NAME ".
@@ -70,68 +66,81 @@ if ($fzgfahrten = $mysqli->query($query)) {
                     "LEFT JOIN haltestellen hs ON hsb.HSTNR = hs.NR ".
                     "WHERE fzpe.LINROUTENAME = '".$row["LINROUTENAME"]."' AND fzpe.FZPROFILNAME = ".$row["FZPROFILNAME"]." ".
                     "ORDER BY fzpe.INDEX ASC ";
-//        printf("%s<br>", $query);
 
-        $batch = array();
         if ($fahrten = $mysqli->query($query)) {
             $count += $fahrten->num_rows;
             $count--;
 
             $row_1 = $fahrten->fetch_assoc();
             while ($row_0 = $fahrten->fetch_assoc()) {
+//                $entry["Ab_HST_Nr"] = $row_1["NR"];
+//                $entry["Ab_HST_Name"] = '"'.$row_1["NAME"].'"';
+//                $entry["ABFAHRT"] = '"'.calc_date($row["ABFAHRT"], $row_1["ABFAHRT"]).'"';
+//                $entry["LINIE"] = $row_1["PUBDIVALINNAM"];
+//                $entry["An_HST_Nr"] = $row_0["NR"];
+//                $entry["An_HST_Name"] = '"'.$row_0["NAME"].'"';
+//                $entry["ANKUNFT"] = '"'.calc_date($row["ABFAHRT"], $row_0["ANKUNFT"]).'"';
+//                $entry["DAUER"] = calc_length($row_1["ABFAHRT"], $row_0["ANKUNFT"]);
+//                $entry["Ab_X"] = '"'.$row_1["XKOORD"].'"';
+//                $entry["Ab_Y"] = '"'.$row_1["YKOORD"].'"';
+//                $entry["An_X"] = '"'.$row_0["XKOORD"].'"';
+//                $entry["An_Y"] = '"'.$row_0["YKOORD"].'"';
+
                 $entry["Ab_HST_Nr"] = $row_1["NR"];
-                $entry["Ab_HST_Name"] = '"'.$row_1["NAME"].'"';
-                $entry["ABFAHRT"] = '"'.calc_date($row["ABFAHRT"], $row_1["ABFAHRT"]).'"';
+                $entry["Ab_HST_Name"] = $row_1["NAME"];
+                $entry["ABFAHRT"] = calc_date($row["ABFAHRT"], $row_1["ABFAHRT"]);
                 $entry["LINIE"] = $row_1["PUBDIVALINNAM"];
                 $entry["An_HST_Nr"] = $row_0["NR"];
-                $entry["An_HST_Name"] = '"'.$row_0["NAME"].'"';
-                $entry["ANKUNFT"] = '"'.calc_date($row["ABFAHRT"], $row_0["ANKUNFT"]).'"';
+                $entry["An_HST_Name"] = $row_0["NAME"];
+                $entry["ANKUNFT"] = calc_date($row["ABFAHRT"], $row_0["ANKUNFT"]);
                 $entry["DAUER"] = calc_length($row_1["ABFAHRT"], $row_0["ANKUNFT"]);
-                $entry["Ab_X"] = '"'.$row_1["XKOORD"].'"';
-                $entry["Ab_Y"] = '"'.$row_1["YKOORD"].'"';
-                $entry["An_X"] = '"'.$row_0["XKOORD"].'"';
-                $entry["An_Y"] = '"'.$row_0["YKOORD"].'"';
+                $entry["Ab_X"] = $row_1["XKOORD"];
+                $entry["Ab_Y"] = $row_1["YKOORD"];
+                $entry["An_X"] = $row_0["XKOORD"];
+                $entry["An_Y"] = $row_0["YKOORD"];
 
                 array_push($batch, $entry);
                 $row_1 = $row_0;
-
-//                if ($first) {
-//                    printf("Erster Eintrag: %s <br>", implode(", ", $entry));
-//                    $first = false;
-//                }
-            }
-
-            $starti = 0;
-            if ($j == 1) {
-                $jquery .= "(".implode(", ",$batch[0]).")";
-                $starti = 1;
-            }
-
-            for ($i = $starti; $i < count($batch); $i++) {
-                $jquery .= ",(".implode(",",$batch[$i]).")";
-            }
-
-            $jcount += count($batch);
-
-            if ($j == 100) {
-                printf("Insert batch with ".$jcount." entries<br>");
-                $mysqli->query($jquery);
-                $j = 0;
-                $jquery = "INSERT INTO im1 (Ab_HST_Nr, Ab_HST_Name, ABFAHRT, LINIE, An_HST_Nr, An_HST_Name, ANKUNFT, DAUER, Ab_X, Ab_Y, An_X, An_Y) VALUES ";
-                $jcount = 0;
             }
 
             /* free result set */
             $fahrten->free();
         }
-
     }
-    printf("Insert batch with ".$jcount." entries<br>");
-    $mysqli->query($jquery);
 
     /* free result set */
-    printf("Profilelemente: %s<br>", $count);
     $fzgfahrten->free();
+
+    $count = count($batch);
+    printf("Allocated " . $count . " rows " . ceil($count / 5000) . "<br><br>");
+
+//    for ($i = 0; $i < ceil($count / 5000); $i++) {
+//        $start = $i * 5000;
+//        $end = $start + 4999;
+//        if ($end > $count-1) {
+//            $end = $count-1;
+//        }
+//
+//        printf("Inserting batch from " . $start . " to " . $end . "<br>");
+//        $jquery = "INSERT INTO im1 (Ab_HST_Nr, Ab_HST_Name, ABFAHRT, LINIE, An_HST_Nr, An_HST_Name, ANKUNFT, DAUER, Ab_X, Ab_Y, An_X, An_Y) VALUES (";
+//        $jquery .= "(" . implode(", ", $batch[$start]) . ")";
+//        printf("Sample row: " . implode(", ", $batch[$start]) . "<br>");
+//        for ($j = $start+1; $j < $end; $j++) {
+//            $jquery .= ", (" . implode(",", $batch[$j]) . ")";
+//        }
+//        $jquery .= ")";
+//    }
+
+
+    $file = fopen("C://xampp/im1.csv","w");
+
+    foreach ($batch as $line)
+    {
+        fputcsv($file,$line,";");
+    }
+
+    fclose($file);
+
 }
 
 /* close connection */
