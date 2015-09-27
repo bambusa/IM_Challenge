94db3bc28d1c891f0c31bdb321d22b272efb2440
bambusa <beny-ulms@gmx.de>
2015-09-27 20:20:23 +0200

    ADD: Relative Paths, Exec. JAR

--- src/IM_Challenge/IM_Challenge.java
+++ src/IM_Challenge/IM_Challenge.java
@@ -43,16 +43,11 @@ public class IM_Challenge {
     public static int NNAME = 5;
     public static int ZEIT = 6;
     // Files
-    private static final String tripCSV = "C:\\Users\\BSD\\IdeaProjects\\IM_Challenge\\PHP Database\\IM_Trips.csv";
-    private static final String transferCSV = "C:\\Users\\BSD\\IdeaProjects\\IM_Challenge\\PHP Database\\IM_Transfers.csv";
-    private static final String resultCSV = "C:\\Users\\BSD\\IdeaProjects\\IM_Challenge\\PHP Database\\IM_Result.csv";
-    private static final String waitingCSV = "C:\\Users\\BSD\\IdeaProjects\\IM_Challenge\\PHP Database\\IM_Long_Waiting_Time.csv";
-    private static final String unnecessaryCSV = "C:\\Users\\BSD\\IdeaProjects\\IM_Challenge\\PHP Database\\IM_Unnecessary_Trips.csv";
-//    private static final String tripCSV = "C://Users/Tristan/IdeaProjects/IM_Challenge/PHP Database/IM_Trips.csv";
-//    private static final String transferCSV = "C://Users/Tristan/IdeaProjects/IM_Challenge/PHP Database/IM_Transfers.csv";
-//    private static final String resultCSV = "C://Users/Tristan/IdeaProjects/IM_Challenge/PHP Database/IM_Result.csv";
-//    private static final String waitingCSV = "C://Users/Tristan/IdeaProjects/IM_Challenge/PHP Database/IM_Long_Waiting_Time.csv";
-//    private static final String unnecessaryCSV = "C://Users/Tristan/IdeaProjects/IM_Challenge/PHP Database/IM_Unnecessary_Trips.csv";
+    private static final String tripCSV = ".\\input\\IM_Trips.csv";
+    private static final String transferCSV = ".\\input\\IM_Transfers.csv";
+    private static final String resultCSV = ".\\output\\IM_Result.csv";
+    private static final String waitingCSV = ".\\output\\IM_Long_Waiting_Time.csv";
+    private static final String unnecessaryCSV = ".\\output\\IM_Unnecessary_Trips.csv";
 
     public static void main(String[] args) {
         long overallTime = System.currentTimeMillis();
